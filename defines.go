b3ca80f34d9af277910f1ce7680ad53ae87038e3
Eric Wang <wangchaogo1990@gmail.com>
2021-07-20 16:41:17 +0800

    rm defines

--- defines.go
+++
@@ -1,9 +0,0 @@
-package errs
-
-import "errors"
-
-// pre defined errors
-var (
-	ErrNotFound         = errors.New("not found")
-	ErrPermissionDenied = errors.New("persmission denied")
-)
--- errs_test.go
+++ errs_test.go
@@ -1,14 +1,20 @@
 package errs_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/ericuni/errs"
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	ErrNotFound         = errors.New("not found")
+	ErrPermissionDenied = errors.New("persmission denied")
+)
+
 func foo() error {
-	return errs.Tracef(errs.ErrNotFound, "user %v", "xxx")
+	return errs.Tracef(ErrNotFound, "user %v", "xxx")
 }
 
 func bar() error {
@@ -19,15 +25,15 @@ func TestTrace_Function(t *testing.T) {
 	t.Log(errs.Trace(bar()))
 }
 
-type _Foo struct {
+type Foo struct {
 }
 
-func (foo *_Foo) sayHi() error {
-	return errs.Trace(errs.ErrPermissionDenied)
+func (foo *Foo) sayHi() error {
+	return errs.Trace(ErrPermissionDenied)
 }
 
 func TestTrace_Method(t *testing.T) {
-	foo := &_Foo{}
+	foo := &Foo{}
 	t.Log(errs.Trace(foo.sayHi()))
 }
 
