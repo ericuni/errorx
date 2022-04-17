c86c5f7ebf4fc25e7873978bcb35747e1855a1c6
liuqi.victor <liuqi.victor@bytedance.com>
2022-04-17 17:15:10 +0800

    add test using errors.Is and errors.As

--- error_test.go
+++ error_test.go
@@ -2,45 +2,65 @@ package errorx_test
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/ericuni/errorx"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-var (
-	ErrNotFound         = errors.New("not found")
-	ErrPermissionDenied = errors.New("persmission denied")
-)
-
-func foo() error {
-	return errorx.Tracef(ErrNotFound, "user %v", "xxx")
+type ErrorSuite struct {
+	suite.Suite
 }
 
-func bar() error {
-	return errorx.Trace(foo())
-}
-
-func TestTrace_Function(t *testing.T) {
-	t.Log(errorx.Trace(bar()))
-}
-
-type Foo struct {
-}
-
-func (foo *Foo) sayHi() error {
-	return errorx.Trace(ErrPermissionDenied)
-}
-
-func TestTrace_Method(t *testing.T) {
-	foo := &Foo{}
-	t.Log(errorx.Trace(foo.sayHi()))
-}
-
-func TestTraceNil(t *testing.T) {
-	assert := assert.New(t)
+func (s *ErrorSuite) TestNil() {
+	assert := s.Assert()
+	// t := s.T()
 
 	var err error = nil
 	assert.Nil(errorx.Trace(err))
 	assert.Nil(errorx.Tracef(err, "something happened"))
+}
+
+func (s *ErrorSuite) TestStandardError() {
+	assert := s.Assert()
+	// t := s.T()
+
+	errNotFound := errors.New("not found")
+
+	err := errorx.Tracef(errNotFound, "something happened")
+
+	assert.True(errors.Is(err, errNotFound))
+}
+
+type MyError struct {
+	Code int32
+	Msg  string
+}
+
+func (e *MyError) Error() string {
+	return fmt.Sprintf("%d %s", e.Code, e.Msg)
+}
+
+func (s *ErrorSuite) TestMyError() {
+	assert := s.Assert()
+	// t := s.T()
+
+	errNotFound := &MyError{
+		Code: http.StatusNotFound,
+		Msg:  "not found",
+	}
+
+	err := errorx.Trace(errNotFound)
+	err = errorx.Tracef(err, "more")
+
+	assert.True(errors.Is(err, errNotFound))
+
+	var myErr *MyError
+	assert.True(errors.As(err, &myErr))
+}
+
+func TestError(t *testing.T) {
+	suite.Run(t, new(ErrorSuite))
 }
