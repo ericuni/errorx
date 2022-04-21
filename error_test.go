626a1e198bb137e789f4d4186e718aa9ad6d0060
liuqi.victor <liuqi.victor@bytedance.com>
2022-04-21 16:53:15 +0800

    add map

--- error_test.go
+++ error_test.go
@@ -3,7 +3,6 @@ package errorx_test
 import (
 	"errors"
 	"fmt"
-	"net/http"
 	"testing"
 
 	"github.com/ericuni/errorx"
@@ -28,28 +27,29 @@ func (s *ErrorSuite) TestStandardError() {
 	// t := s.T()
 
 	errNotFound := errors.New("not found")
-
 	err := errorx.Tracef(errNotFound, "something happened")
-
 	assert.True(errors.Is(err, errNotFound))
+
+	errNone := errors.New("none")
+	assert.False(errors.Is(err, errNone))
 }
 
 type PointerError struct {
-	Code int32
-	Msg  string
+	code int32
+	msg  string
 }
 
 func (e *PointerError) Error() string {
-	return fmt.Sprintf("%d %s", e.Code, e.Msg)
+	return fmt.Sprintf("%d %s", e.code, e.msg)
 }
 
 type ValueError struct {
-	Code int32
-	Msg  string
+	code int32
+	msg  string
 }
 
 func (e ValueError) Error() string {
-	return fmt.Sprintf("%d %s", e.Code, e.Msg)
+	return fmt.Sprintf("%d %s", e.code, e.msg)
 }
 
 func (s *ErrorSuite) TestMyError() {
@@ -58,8 +58,8 @@ func (s *ErrorSuite) TestMyError() {
 
 	t.Run("pointer", func(t *testing.T) {
 		errNotFound := &PointerError{
-			Code: http.StatusNotFound,
-			Msg:  "not found",
+			code: -1,
+			msg:  "not found",
 		}
 
 		err := errorx.Trace(errNotFound)
@@ -69,12 +69,18 @@ func (s *ErrorSuite) TestMyError() {
 
 		var myErr *PointerError
 		assert.True(errors.As(err, &myErr))
+
+		errNone := &PointerError{
+			code: -2,
+			msg:  "none",
+		}
+		assert.False(errors.Is(err, errNone))
 	})
 
 	t.Run("value", func(t *testing.T) {
 		errNotFound := ValueError{
-			Code: http.StatusNotFound,
-			Msg:  "not found",
+			code: -1,
+			msg:  "not found",
 		}
 
 		err := errorx.Trace(errNotFound)
@@ -84,6 +90,83 @@ func (s *ErrorSuite) TestMyError() {
 
 		var myErr ValueError
 		assert.True(errors.As(err, &myErr))
+
+		errNone := ValueError{
+			code: -2,
+		}
+		assert.False(errors.Is(err, errNone))
+	})
+}
+
+// https://go.dev/ref/spec#Comparison_operators
+// Slice, map, and function values are not comparable.
+type PointerErrorWithMap struct {
+	code  int32
+	msg   string
+	extra map[string]string
+}
+
+func (e *PointerErrorWithMap) Error() string {
+	return fmt.Sprintf("%d %s %v", e.code, e.msg, e.extra)
+}
+
+type ValueErrorWithMap struct {
+	code  int32
+	msg   string
+	extra map[string]string
+}
+
+func (e ValueErrorWithMap) Error() string {
+	return fmt.Sprintf("%d %s %v", e.code, e.msg, e.extra)
+}
+
+func (s *ErrorSuite) TestNotComparable() {
+	assert := s.Assert()
+	t := s.T()
+
+	t.Run("pointer", func(t *testing.T) {
+		errNotFound := &PointerErrorWithMap{
+			code:  -1,
+			msg:   "not found",
+			extra: map[string]string{"name": "xxx"},
+		}
+
+		err := errorx.Trace(errNotFound)
+		err = errorx.Tracef(err, "more")
+
+		assert.True(errors.Is(err, errNotFound))
+
+		var myErr *PointerErrorWithMap
+		assert.True(errors.As(err, &myErr))
+
+		errNone := &PointerErrorWithMap{
+			code: -2,
+			msg:  "none",
+		}
+		assert.False(errors.Is(err, errNone))
+	})
+
+	t.Run("value", func(t *testing.T) {
+		errNotFound := ValueErrorWithMap{
+			code:  -1,
+			msg:   "not found",
+			extra: map[string]string{"name": "xxx"},
+		}
+
+		err := errorx.Trace(errNotFound)
+		err = errorx.Tracef(err, "more")
+
+		// map is not comparable, but errors.Is would use the comparability
+		assert.False(errors.Is(err, errNotFound))
+
+		var myErr ValueErrorWithMap
+		assert.True(errors.As(err, &myErr))
+
+		errNone := ValueErrorWithMap{
+			code: -2,
+			msg:  "none",
+		}
+		assert.False(errors.Is(err, errNone))
 	})
 }
 
